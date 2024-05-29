b71b7cd44afd230d4a3a39224c14e1e76bd0f2e7
estivenVillanueva <168735734+estivenVillanueva@users.noreply.github.com>
2024-05-28 19:25:54 -0500

    setup inicial proyecto practica array

---
+++ index.js
@@ -0,0 +1,407 @@
+let libros=[
+    {   titulo:"orgullo y prejuicio",
+        numPaginas:150,
+        autor:"Jane Austen",
+        genero:"Romance de regencia,Ficción de aventuras",
+        idioma:"ingles",
+        precio:"$"+248,
+        formato:"Tapa blanda",
+        isbn:9780140430721,
+        descripcion:"Orgullo y prejuicio narra las aventuras y desventuras amorosas<br> de las hermanas Bennet, centrándose en el personaje de Elizabeth",
+        estado:"Nuevo",
+        ubicacion:"Londres",
+        fecha_publicacion:"28 de enero de 1813",
+        dimensiones:"5.2 x 1 x 8 pulgadas",
+        peso:"50grs",
+        editorial:" Editorial Alma",
+    },
+    {
+        titulo:"Cumbres borrascosas",
+        numPaginas:456,
+        autor:"Emily Bronte ",
+        genero:"Tragedia",
+        idioma:"Español",
+        precio:"$"+50.5,
+        formato:"Pasta dura",
+        isbn:9780007326747,
+        descripcion:"Cumbres borrascosas, una de las novelas inglesas más relevantes del<br>siglo XIX, narra la épica historia de Catherine y Heathcliff.",
+        estado:"usado",
+        ubicacion:"Inglaterra",
+        fecha_publicacion:"diciembre"+1847,
+        dimensiones:"14 x 2.8 x 21 cm",
+        peso:756+"gr",
+        editorial:"Editorial Alma",
+    },
+    {
+        titulo:"Jane Eyre",
+        numPaginas:	456,
+        autor:"Charlotte Bronte",
+        genero:"Ficción,Poesía",
+        idioma:"ingles",
+        precio:"$"+1.638,
+        formato:"pata dura",
+        isbn:9789588979519,
+        descripcion:"Una de las grandes novelas de todos los tiempos.<br> La obra maestra de la literatura victoriana. Jane Eyre es una novela clásica de amor, precursora del feminismo y la psicología moderna, sobre una<br> huérfana que enfrenta su destino manifesto gracias a su inteligencia e integridad inquebrantables.",
+        estado:"Nuevo",
+        ubicacion:"Irlanda",
+        fecha_publicacion:19+"de octubre de"+1847,
+        dimensiones:"5.08 x 1.02 x 7.8 pulgadas",
+        peso:50+"grs",
+        editorial:"Editorial Alma"
+    },
+    {
+        titulo:"Anna karenina",
+        numPaginas:190,
+        autor:"Leon Tolstoi",
+        genero:"novela,Ficcion",
+        idioma:"ruso",
+        precio:"$"+500,
+        formato:"tapa dura",
+        isbn:9788484284925,
+        descripcion:" La novela narra la trágica historia de Anna Karenina, una joven aristócrata que tiene un romance<br>ilícito y todas las consecuencia sociales y personales de su indiscreción.",
+        estado:"nuevo",
+        ubicacion:"Rusia",
+        fecha_publicacion:1877,
+        dimensiones:"5.0 x 1.08 x 7.8 pulgadas",
+        peso:60+"grs",
+        editorial:"Editorial Alma",
+    },
+    {
+        titulo:"+18",
+        numPaginas:"200",
+        autor:"Darlis Stefany",
+        genero:"Infantil y juveni",
+        idioma:"Español",
+        precio:"$"+60,
+        formato:"pasta balnda",
+        isbn:98676546543,
+        descripcion:"Una historia de amor alocada, divertida y llena de giros inesperados. Alaska Hans tiene un secreto: le encanta escribir bajo seudónimo y se ha convertido en un éxito en la plataforma JoinApp.",
+        estado:"Nuevo",
+        ubicacion:"No especificado",
+        fecha_publicacion:"2022",
+        dimensiones:"No espesificado",
+        peso:"No espesificado",
+        editorial:"Editorial Alma",
+    },
+    {
+        titulo:"Las mil y una noches",
+        numPaginas:159,
+        autor:"Anónimo",
+        genero:"Novela literaria",
+        idioma:" Español",
+        precio:"$"+70,
+        formato:"pasta dura",
+        isbn:974522435477,
+        descripcion:"Un clásico consistente en una recopilación de cuentos tradicionales de Oriente Medio, con historias como Simbad y Alí Babá y los cuarenta ladrones.",
+        estado:"Nuevo",
+        ubicacion:" No especificado",
+        fecha_publicacion:"Siglo XVIII",
+        dimensiones:"6.0 x 1.08 x 7.8 pulgadas",
+        peso:55+"grs", 
+        editorial:"HarperCollins", 
+    },
+    {
+        titulo:"Cien años de soledad",
+        numPaginas:471,
+        autor:"Gabriel García Márquez",
+        genero:"Realismo mágico",
+        idioma:"Español",
+        precio:"$"+70,
+        formato:"Tapa blanda y digital",
+        isbn:9780307474728,
+        descripcion:" La obra maestra de Gabriel García Márquez narra la historia de la familia Buendía a lo largo de siete generaciones en el pueblo ficticio de Macondo",
+        estado:"Nuevo o usado",
+        ubicacion:"Disponible en varias librerías y plataformas online",
+        fecha_publicacion:"Primera edición en 1967",
+        dimensiones:"13.97 x 2.54 x 20.96 cm",
+        peso:" 340 g", 
+        editorial:"HarperCollins", 
+    },
+    {
+        titulo:"El amor en los tiempos del cólera",
+        numPaginas:348,
+        autor:"Gabriel García Márquez",
+        genero:" Novela",
+        idioma:"Español",
+        precio:"$"+100,
+        formato:"Tapa blanda y digital",
+        isbn:9780307387264,
+        descripcion:"Esta novela cuenta la vida de Florentino Ariza y su amor no correspondido por Fermina Daza durante más de 50 años.",
+        estado:"Nuevo o usado",
+        ubicacion:" Disponible en varias librerías y plataformas online",
+        fecha_publicacion:"Primera edición en 1985",
+        dimensiones:"13.21 x 2.03 x 20.32 cm",
+        peso:249+"g", 
+        editorial:" HarperCollins", 
+    },
+    {
+        titulo:"La sombra del viento",
+        numPaginas:487,
+        autor:"Carlos Ruiz Zafón",
+        genero:"Novela literaria",
+        idioma:" Español",
+        precio:"$"+50,
+        formato:"Tapa blanda y digital",
+        isbn:9788408079545,
+        descripcion:"Ambientada en la Barcelona de la posguerra, esta novela sigue a un joven que descubre un libro misterioso, lo que lo lleva a una búsqueda a través de los secretos oscuros de la ciudad",
+        estado:"Nuevo o usado",
+        ubicacion:"Disponible en varias librerías y plataformas online",
+        fecha_publicacion:"Primera edición en 2001",
+        dimensiones:"13 x 3.2 x 19.5 cm",
+        peso:320 +"g", 
+        editorial:"HarperCollins", 
+    },
+    {
+        titulo:"El Principito",
+        numPaginas:96,
+        autor:"Antoine de Saint-Exupéry",
+        genero:"Novela corta, cuento filosófico",
+        idioma:"Español",
+        precio:"$"+80,
+        formato:"Tapa blanda, tapa dura, digital",
+        isbn: 9780156013987,
+        descripcion:"Un piloto se encuentra varado en el desierto del Sahara después de que su avión sufriera una avería, pero ahí conoce a un pequeño príncipe proveniente de otro planeta.",
+        estado:"Nuevo o usado",
+        ubicacion:"Disponible en varias librerías y plataformas online",
+        fecha_publicacion:"Primera edición en 1943",
+        dimensiones:22+ "x"+ 14 +"cm",
+        peso:159 +"g", 
+        editorial:"HarperCollins", 
+    },
+    {
+        titulo: 1984,
+        numPaginas:328,
+        autor:"George Orwell",
+        genero:"Ciencia ficción, novela política",
+        idioma:"Español",
+        precio:"$"+50,
+        formato:"Tapa blanda, tapa dura, digital",
+        isbn:9780451524935,
+        descripcion:"En un mundo de vigilancia constante y totalitarismo, Winston Smith lucha por la libertad y la verdad en una sociedad que no permite ninguna de las dos.",
+        estado:"Nuevo",
+        ubicacion:"Disponible en varias librerías y plataformas online",
+        fecha_publicacion:"Primera edición en 1949",
+        dimensiones:17.1+ "x"+ 10.6 +"cm",
+        peso:181 +"g", 
+        editorial:"Harcourt, Brace and Company", 
+    },
+    {
+        titulo:"Don Quijote de la Mancha",
+        numPaginas:1000,
+        autor:"Miguel de Cervantes",
+        genero:"Novela de caballerías, sátira",
+        idioma:"Español",
+        precio:"$"+1600,
+        formato:"Tapa blanda, tapa dura, digital",
+        isbn:9788420412146,
+        descripcion:"La historia del hidalgo que, tras leer demasiados libros de caballerías, decide convertirse en caballero andante y vivir sus propias aventuras junto a su fiel escudero, Sancho Panza",
+        estado:"Nuevo ",
+        ubicacion:"Disponible en varias librerías y plataformas online",
+        fecha_publicacion:" Primera parte en"+ 1605+ "segunda parte en"+1615,
+        dimensiones:" Varían según la edición",
+        peso:" Varían según la edición", 
+        editorial:" Varían según la edición", 
+    },
+    {
+        titulo:"El Código Da Vinci",
+        numPaginas:250,
+        autor:"Dan Brown",
+        genero:"Misterio, Thriller",
+        idioma:" Español",
+        precio:"$"+190,
+        formato:"Disponible en tapa dura, tapa blanda y eBook",
+        isbn:"Variable según la edición",
+        descripcion:"Un libro que combina misticismo, ocultismo e historia, causando gran controversia y convirtiéndose en un bestseller",
+        estado:"Nuevo",
+        ubicacion:"Disponible en varias librerías y plataformas online",
+        fecha_publicacion:"Primera edición en" +2003,
+        dimensiones:15.1 +"x"+ 10.6 +"cm",
+        peso:+65+"g", 
+        editorial:"Doubleday (edición original)", 
+    },
+    {
+        titulo:"El Señor de los Anillos",
+        numPaginas:1200 ,
+        autor:"J.R.R. Tolkien",
+        genero:"Fantasía",
+        idioma:"Disponible en varios idiomas",
+        precio:"$"+2500,
+        formato:"Disponible en tapa dura, tapa blanda y digital",
+        isbn:987365226252,
+        descripcion:"Una épica trilogía de fantasía que es considerada un clásico de la literatura.",
+        estado:"Nuevo",
+        ubicacion:"Disponible en librerías y plataformas en línea",
+        fecha_publicacion:1954-1955,
+        dimensiones:15.1 +"x"+ 10.6 +"cm",
+        peso:80+"g", 
+        editorial:"George Allen & Unwin", 
+    },
+    {
+        titulo:"El Diario de Ana Frank",
+        numPaginas:352,
+        autor:"Ana Frank",
+        genero:" Biografía, Autobiografía",
+        idioma:"Disponible en varios idiomas",
+        precio:"$"+500,
+        formato:"Disponible en tapa dura, tapa blanda y digital",
+        isbn:963542108636,
+        descripcion:"El conmovedor diario de una joven durante su ocultamiento en la Segunda Guerra Mundial",
+        estado:"Nuevo",
+        ubicacion:"Disponible en librerías",
+        fecha_publicacion:1947,
+        dimensiones:13.1 +"x"+ 11.6 +"cm",
+        peso:45+"g", 
+        editorial:"Contact Publishing", 
+    },
+    {
+        titulo:"Matar a un Ruiseñor",
+        numPaginas: 281,
+        autor:"Harper Lee",
+        genero:"Novela, Drama",
+        idioma:"Disponible en varios idiomas",
+        precio:"$"+75,
+        formato:"Disponible en tapa dura, tapa blanda y digital",
+        isbn:982442644262,
+        descripcion:"Una novela que aborda temas de racismo y justicia a través de los ojos de una niña en el sur de Estados Unidos",
+        estado:"Nuevo",
+        ubicacion:"Disponible en librerías ",
+        fecha_publicacion:1960,
+        dimensiones:18.1 +"x"+ 16.6 +"cm",
+        peso:35+"g", 
+        editorial:"J.B. Lippincott & Co", 
+    },
+    {
+        titulo:"Lolita",
+        numPaginas:336,
+        autor:"Vladimir Nabokov",
+        genero:" Novela",
+        idioma:"Disponible en varios idiomas",
+        precio:"$"+65,
+        formato:"Disponible en tapa dura, tapa blanda y digital",
+        isbn:97357255222,
+        descripcion:"Una polémica obra maestra que explora la obsesión y la tragedia a través de la relación entre el protagonista y una joven adolescente",
+        estado:"Nuevo",
+        ubicacion:"Disponible en librerías y plataformas en línea",
+        fecha_publicacion:1955,
+        dimensiones:18.1 +"x"+ 17.6 +"cm",
+        peso:76+"g", 
+        editorial:" Olympia Press", 
+    },
+    {
+        titulo:"El Viejo y el Mar",
+        numPaginas:128,
+        autor:" Ernest Hemingway",
+        genero:" Novela, Literatura clásica",
+        idioma:"Disponible en varios idiomas",
+        precio:"$"+67,
+        formato:"Disponible en tapa dura",
+        isbn:98725326252,
+        descripcion:"La historia de un viejo pescador y su lucha épica con un gran pez marlín",
+        estado:"Usado",
+        ubicacion:" Disponible en librerías",
+        fecha_publicacion:1952,
+        dimensiones:10.1 +"x"+ 12.6 +"cm",
+        peso:+30+"g", 
+        editorial:" Charles Scribner’s Sons", 
+    },
+    {
+        titulo:"La Historia Interminable",
+        numPaginas:396,
+        autor:"Michael Ende",
+        genero:"Fantasía, Aventura",
+        idioma:"Disponible en varios idiomas",
+        precio:"$"+69,
+        formato:"tapa blanda",
+        isbn:98634245242,
+        descripcion:" Un libro dentro de un libro, donde un niño lee sobre un reino de fantasía que está desapareciendo y descubre que él es parte de la historia.",
+        estado:"Nuevo",
+        ubicacion:"plataformas en línea",
+        fecha_publicacion:1979,
+        dimensiones:12.1 +"x"+ 12.6 +"cm",
+        peso:35+"g", 
+        editorial:"Thienemann Verlag", 
+    },
+    {
+        titulo:"El nombre de la rosa",
+        numPaginas:512,
+        autor:"Umberto Eco",
+        genero:" Misterio, Histórico",
+        idioma:" Originalmente en italiano, disponible en varios idiomas",
+        precio:"$"+700,
+        formato:"tapa dura",
+        isbn:9852157885,
+        descripcion:"Un monje investiga una serie de misteriosas muertes en una abadía en Italia durante la Edad Media.",
+        estado:"Usado",
+        ubicacion:"Disponible en librerías",
+        fecha_publicacion:1980,
+        dimensiones:16.1 +"x"+ 13.6 +"cm",
+        peso:80+"g", 
+        editorial:"Bompiani", 
+    },
+];
+function agregarLibro(libro) {
+    libros.push(libro);
+    console.log(`El libro "${libro.titulo}" ha sido agregado a la pila.`);
+  }
+  
+
+  function quitarLibro() {
+    if (libros.length === 0) {
+      console.log("La pila está vacía.");
+    } else {
+      const libroQuitado = libros.pop();
+      console.log(`El libro "${libroQuitado.titulo}" ha sido quitado de la pila.`);
+    }
+  }
+  function mostrarPila() {
+    if (libros.length === 0) {
+      console.log("La pila está vacía.");
+    } else {
+      console.log("Libros en la pila:");
+      libros.forEach((libro, index) => {
+        console.log(`${index + 1}. ${libro.titulo}`);
+      });
+    }
+  }
+  
+
+     
+    function mostrarMenu() {
+    console.log("/nMenú de la pila de libros:");
+    console.log("1. Agregar un libro");
+    console.log("2. Quitar un libro");
+    console.log("3. Mostrar la pila");
+    console.log("4. Salir");
+   }
+
+    function iniciarPila() {
+    let opcion = 0;
+  
+    while (opcion !== 4) {
+      mostrarMenu();
+      opcion = parseInt(prompt("Ingrese una opción 1.agregar libro,2.Quitar libro,3.Mostrar la pila,4.salir : "));
+  
+      switch (opcion) {
+        case 1:
+          const nuevoLibro = {
+            titulo: prompt("ingrese el titulo del libro"),
+
+          };
+          agregarLibro(nuevoLibro);
+          break;
+        case 2:
+          quitarLibro();
+          break;
+        case 3:
+          mostrarPila();
+          break;
+        case 4:
+          console.log("¡Hasta luego!");
+          break;
+        default:
+          console.log("Opción inválida. Intente nuevamente.");
+      }
+    }
+  }
+  
+  iniciarPila()
